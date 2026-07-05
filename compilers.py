214c4b52655b5fab551f39eba679941ff588d8c1
Xavier Leclercq <xavier@leclercq.dev>
2026-07-05 13:15:05 +0100

    Drop support for vc14 and vc15  and fix architecture selection

--- compilers.py
+++ compilers.py
@@ -3,11 +3,15 @@ import subprocess
 
 
 class Compiler:
-    def __init__(self, name, short_name, executable, cmake_generator):
+    def __init__(self, name, short_name, executable, cmake_generator,
+                 cmake_platform=None):
         self.name = name
         self.short_name = short_name
         self.executable = executable
         self.cmake_generator = cmake_generator
+        # The CMake "-A" platform argument (e.g. "x64" / "Win32"), or None for
+        # generators that don't take one (e.g. Unix Makefiles).
+        self.cmake_platform = cmake_platform
 
     def compile(self, makefile_path, configuration, input):
         try:
@@ -35,20 +39,18 @@ class GNUmake(Compiler):
 class VisualStudio(Compiler):
     def __init__(self, name, short_name, executable, architecture):
         cmake_generator = ""
-        if short_name == "vc14":
-            cmake_generator = "Visual Studio 14 2015"
-        elif short_name == "vc15":
-            cmake_generator = "Visual Studio 15 2017"
-        elif short_name == "vc17":
+        if short_name == "vc17":
             cmake_generator = "Visual Studio 17 2022"
         elif short_name == "vc18":
             cmake_generator = "Visual Studio 18 2026"
-        # The " Win64" generator suffix only exists for VS2017 and earlier.
-        # VS2019+ (e.g. vc18) select the architecture with the "-A" argument
-        # and otherwise default to the host platform.
-        if architecture == "64" and short_name in ("vc14", "vc15"):
-            cmake_generator += " Win64"
-        super().__init__(name, short_name, executable, cmake_generator)
+        # VS2019+ generators select the architecture with the "-A" argument
+        # rather than a generator-name suffix.
+        if architecture == "64":
+            cmake_platform = "x64"
+        else:
+            cmake_platform = "Win32"
+        super().__init__(name, short_name, executable, cmake_generator,
+                         cmake_platform)
 
     def compile(self, makefile_path, configuration, input):
         try:
@@ -73,12 +75,6 @@ class Compilers:
         foundMSVC2022 = os.path.isfile("C:/Program Files/Microsoft Visual Studio/2022/Professional/Common7/IDE/devenv.exe")
         if foundMSVC2022:
             self.compilers.append(VisualStudio("Visual Studio 2022", "vc17", "C:/Program Files/Microsoft Visual Studio/2022/Professional/Common7/IDE/devenv.exe", self.architecture))
-        foundMSVC2017 = os.path.isfile("C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe")
-        if foundMSVC2017:
-            self.compilers.append(VisualStudio("Visual Studio 2017", "vc15", "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe", self.architecture))
-        foundMSVC14 = os.path.isfile("C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe")
-        if foundMSVC14:
-            self.compilers.append(VisualStudio("Visual Studio 2015", "vc14", "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe", self.architecture))
         if target.platform == "Linux":
             self.compilers.append(GNUmake())
         
